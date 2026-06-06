eff2b3fa433a58c6555c2cc332c5cf03a78ed4b6
Gal Dadon <avramludor@gmail.com>
2026-06-06 16:04:35 +0300

    Remove COEP header to allow Google Drive iframes

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "Cross-Origin-Embedder-Policy", value: "credentialless" },
-          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
